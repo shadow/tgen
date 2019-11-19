2ce30175b05fdb0376d577f921ffc718af664c10
Rob Jansen <jansen@cs.umn.edu>
2019-11-19 12:58:09 -0500

    Upgrade new stream log message to info level

--- src/tgen-stream.c
+++ src/tgen-stream.c
@@ -2006,7 +2006,7 @@ TGenStream* tgenstream_new(const gchar* idStr, TGenStreamOptions* options,
         _tgenstream_changeRecvState(stream, TGEN_STREAM_RECV_AUTHENTICATE);
     }
 
-    tgen_debug("Created new stream %s on transport %s",
+    tgen_info("Created new stream %s on transport %s",
             _tgenstream_toString(stream), tgentransport_toString(transport));
 
     stream->time.nowCached = 0;
