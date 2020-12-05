0a00629a21fe4647d4d868ee7f65511edc5669e8
florian-vuillemot <florian.vuillemot@posteo.net>
2020-12-05 19:05:15 +0100

    Convert script test from python2 to python3

--- test/expected-results/plot-results.sh
+++ test/expected-results/plot-results.sh
@@ -2,7 +2,7 @@
 # note that the plot script requires matplotlib, numpy, and scipy
 for seed in 123 321
 do
-	python ../../tools/scripts/plot-dist.py ${seed}
+	python3 ../../tools/scripts/plot-dist.py ${seed}
 done
 
 echo "See the PDF files in the build-test directory"
