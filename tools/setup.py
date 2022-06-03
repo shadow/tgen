bcb36ea3797ca0029aa9a7fb3b7b8f24d47bfb17
Rob Jansen <jansen@cs.umn.edu>
2022-06-03 18:43:47 -0400

    Bump version: 1.0.0 → 1.1.0

--- tools/setup.py
+++ tools/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='TGenTools',
-      version="1.0.0",
+      version="1.1.0",
       description='A utility to analyze and visualize TGen output',
       author='Rob Jansen',
       url='https://github.com/shadow/tgen/',
